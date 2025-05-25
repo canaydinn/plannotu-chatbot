f9a9d7af416f5d3b36fc01504a2feef77249be11
Can Aydın <canaydinn@gmail.com>
2025-05-25 18:59:33 +0300

    Update embeddings.js

--- utils/embeddings.js
+++ utils/embeddings.js
@@ -10,10 +10,6 @@ export async function getEmbedding(text) {
     model: "text-embedding-3-small",
     input: text,
   });
-
   return response.data[0].embedding;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9ec22e01c65864020db0dec70cdbc7f8e68f39a9
+
