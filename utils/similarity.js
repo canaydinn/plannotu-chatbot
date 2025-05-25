cf7742dede90209f10f68200933eb638e31bc875
Can Aydın <canaydinn@gmail.com>
2025-05-25 18:53:30 +0300

    add:utils dosyaları

---
+++ utils/similarity.js
@@ -0,0 +1,9 @@
+// utils/similarity.js
+
+// İki vektör arasındaki kosinüs benzerliğini hesaplar
+export function cosineSimilarity(vecA, vecB) {
+  const dotProduct = vecA.reduce((sum, a, i) => sum + a * vecB[i], 0);
+  const normA = Math.sqrt(vecA.reduce((sum, a) => sum + a * a, 0));
+  const normB = Math.sqrt(vecB.reduce((sum, b) => sum + b * b, 0));
+  return dotProduct / (normA * normB);
+}
